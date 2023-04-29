264d8bf20440b2c6f83c8fe4dfbbcdcd6254e97a
Basavaraj-PN <bnarasapur@gmail.com>
2023-04-29 11:59:09 +0530

    removed extra lines

--- demo.py
+++ demo.py
@@ -6,11 +6,9 @@ client = carla.Client('localhost', 2000)
 client.set_timeout(2.0)
 world = client.get_world()
 
-
 ego_player_id = 406
 
 while True:
-
     filtered_actors = actor_sensor(ego_player_id=ego_player_id, world=world,
                                    h_fov=45, v_fov=5, show_transform=True, range_=30, actor_filter='vehicle.*')
     for actor in filtered_actors:
